b09bb83407ca343fabd1f0fdf1bdaf714f58574b
Toni Sfetcu <toni.sfetcu@brainworks.ro>
2025-11-26 20:00:40 +0200

    cleanup, package update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,54 +1,61 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
-import compress from "astro-compress";
-import AstroPWA from '@vite-pwa/astro';
+import AstroPWA from "@vite-pwa/astro";
 import { seoconfig } from "./utils/seoconfig";
 
 // https://astro.build/config
 export default defineConfig({
   site: seoconfig.baseURL,
   build: {
-    assets: "assets"
+    assets: "assets",
   },
   vite: {
-    logLevel: 'info',
+    logLevel: "info",
     define: {
-      __DATE__: `'${new Date().toISOString()}'`
-    }
+      __DATE__: `'${new Date().toISOString()}'`,
+    },
   },
-  integrations: [mdx(), compress(), AstroPWA({
-    mode: 'development',
-    base: '/',
-    scope: '/',
-    includeAssets: ['favicon.svg'],
-    manifest: {
-      name: 'Numberbee Game by Voltable.com',
-      short_name: 'Numberbee Game',
-      theme_color: '#ffffff',
-      start_url: '/',
-      icons: [{
-        src: 'favicon-192.png',
-        sizes: '192x192',
-        type: 'image/png'
-      }, {
-        src: 'favicon-512.png',
-        sizes: '512x512',
-        type: 'image/png'
-      }, {
-        src: 'favicon-512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable'
-      }]
-    },
-    workbox: {
-      globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}']
-    },
-    devOptions: {
-      enabled: true,
-      navigateFallback: '/404'
-    }
-  })]
+  integrations: [
+    mdx(),
+    AstroPWA({
+      mode: "development",
+      base: "/",
+      scope: "/",
+      includeAssets: ["favicon.svg"],
+      manifest: {
+        name: "Numberbee Game by Voltable.com",
+        short_name: "Numberbee Game",
+        theme_color: "#ffffff",
+        start_url: "/",
+        icons: [
+          {
+            src: "favicon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "favicon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "favicon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallback: "/404",
+      },
+    }),
+  ],
 });
+
